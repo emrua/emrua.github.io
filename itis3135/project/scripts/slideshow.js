4b6dc17a70adae35757e7c31d2e8dfb0883be46b
Emma Anglin <eanglin2@uncc.edu>
2024-05-05 14:49:18 -0400

    updates

--- itis3135/project/scripts/slideshow.js
+++ itis3135/project/scripts/slideshow.js
@@ -1,29 +1,32 @@
-let currentSlide = 0;
-const slides = document.getElementsByClassName("slide");
+$(document).ready(function(){
+    var currentIndex = 0;
+    var images = [
+        {src: "images/Construction.jpg", caption: 'Updated porch and fence with stairs and a ramp'},
+        {src: 'images/ramp.jpg', caption: 'Close up of the ramp and stairs'},
+        {src: 'images/counter.jpg', caption: 'Built in counter and dishwasher cover'},
+        {src: 'images/doghouse.jpg', caption: 'Homemade dog house'},
+        {src: 'images/fence.jpg', caption: 'Privacy fence around porch'},
+        {src: 'images/pingpong1.jpg', caption: 'Full size ping pong table'},
+        {src: 'images/pingpong2.jpg', caption: 'Another angle of the ping pong table'},
+        
+        
+    ];
 
-function showSlide(index) {
-    // Hide all slides
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    function showImage(index) {
+        $('#slide-img').attr('src', images[index].src);
+        $('#caption').text(images[index].caption);
+        $('#thumbnails li').removeClass('active');
+        $('#thumbnails li').eq(index).addClass('active');
+        currentIndex = index;
     }
 
-    // Ensure the index is within bounds
-    currentSlide = (index + slides.length) % slides.length;
+    images.forEach(function(image, index) {
+        var thumbnail = $('<li>').appendTo('#thumbnails');
+        var img = $('<img>').attr('src', image.src).appendTo(thumbnail);
+        thumbnail.click(function(){
+            showImage(index);
+        });
+    });
 
-    // Show the current slide
-    slides[currentSlide].style.display = "block";
-}
-
-function nextSlide() {
-    showSlide(currentSlide + 1);
-}
-
-function previousSlide() {
-    showSlide(currentSlide - 1);
-}
-
-// Automatically advance to the next slide every 5 seconds
-setInterval(nextSlide, 10000); // Change interval time as needed
-
-// Show the first slide on page load
-showSlide(0);
+    showImage(currentIndex);
+});
